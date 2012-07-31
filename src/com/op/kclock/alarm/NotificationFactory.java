701d0e30a0f479714e606f73b6dc258c2aea394b
Alexander Pastukhov <picaro.vs@gmail.com>
2012-07-31 14:38:05 +0300

    Update src/com/op/kclock/alarm/NotificationFactory.java

--- src/com/op/kclock/alarm/NotificationFactory.java
+++ src/com/op/kclock/alarm/NotificationFactory.java
@@ -1,4 +1,4 @@
-package com.op.cooker.alarm;
+package com.op.kclock.alarm;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -6,8 +6,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 //import ohtu.beddit.activity.MainActivity;
-import com.op.cooker.utils.TimeUtils;
-import com.op.cooker.*;
+import com.op.kclock.utils.TimeUtils;
+import com.op.kclock.*;
 
 
 /*
