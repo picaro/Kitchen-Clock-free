aa5ec8adab8074535e5af730e3c74eea39508129
Alexander Pastukhov <picaro.vs@gmail.com>
2012-08-10 13:30:28 +0300

    Update src/com/op/kclock/alarm/ClockService.java

--- src/com/op/kclock/alarm/ClockService.java
+++ src/com/op/kclock/alarm/ClockService.java
@@ -1,5 +1,37 @@
 package com.op.kclock.alarm;
 
-public class ClockService
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class ClockService extends Service
 {
+  
+  final String LOG_TAG = "myLogs";
+  
+    public void onCreate() {
+    super.onCreate();
+    Log.d(LOG_TAG, "onCreate");
+  }
+  
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    Log.d(LOG_TAG, "onStartCommand");
+    someTask();
+    return super.onStartCommand(intent, flags, startId);
+  }
+
+  public void onDestroy() {
+    super.onDestroy();
+    Log.d(LOG_TAG, "onDestroy");
+  }
+
+  public IBinder onBind(Intent intent) {
+    Log.d(LOG_TAG, "onBind");
+    return null;
+  }
+  
+  void someTask() {
+  }
+  
 }
