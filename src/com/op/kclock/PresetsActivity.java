8d0664f2b3df85a30eb7b57b39ff72abedb1b09a
Alexander Pastukhov <picaro.vs@gmail.com>
2012-09-19 00:09:21 +0300

    v 8 :)  slide effect

--- src/com/op/kclock/PresetsActivity.java
+++ src/com/op/kclock/PresetsActivity.java
@@ -24,100 +24,92 @@ import com.op.kclock.dialogs.TimePickDialog;
 import android.view.*;
 import com.op.kclock.utils.DBHelper;
 
-public class PresetsActivity extends Activity implements OnClickListener
-{
+public class PresetsActivity extends Activity implements OnClickListener {
 
 	/** Called when the activity is first created. */
-	private List<AlarmClock> histories  = null;
+	private List<AlarmClock> histories = null;
 	private List<AlarmClock> presets = null;
 	private final Map<View, AlarmClock> historyMap = new HashMap<View, AlarmClock>();
-	//private Map<View, AlarmClock> presetsMap = new HashMap<View, AlarmClock>();
+	// private Map<View, AlarmClock> presetsMap = new HashMap<View,
+	// AlarmClock>();
 	private GestureDetector gestureDetector;
-	
+
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		//Bundle savedInstanceState = null;
+	public void onCreate(Bundle savedInstanceState) {
+		// Bundle savedInstanceState = null;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.presets);
-		final LinearLayout presetsList = (LinearLayout)findViewById(R.id.presets_list);
+		final LinearLayout presetsList = (LinearLayout) findViewById(R.id.presets_list);
 		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 
 		DBHelper dbHelper = new DBHelper(getApplicationContext());
 		dbHelper.open();
 
-		//Show presets list
+		// Show presets list
 		presets = dbHelper.getPresetsList();
-		for (final AlarmClock alarm : presets)
-		{
-			
-			String name = (alarm.getName() == null || alarm.getName().length() == 0) ?
-				"alarm": alarm.getName(); 	
-			final View convertView = inflater.inflate(R.layout.preset_unit, null);
-			final TextView tvName = (TextView) convertView.findViewById(R.id.name);
+		for (final AlarmClock alarm : presets) {
+
+			String name = (alarm.getName() == null || alarm.getName().length() == 0) ? "alarm"
+					: alarm.getName();
+			final View convertView = inflater.inflate(R.layout.preset_unit,
+					null);
+			final TextView tvName = (TextView) convertView
+					.findViewById(R.id.name);
 			tvName.setText(name);
-			final TextView tvTime = (TextView) convertView.findViewById(R.id.time);
+			final TextView tvTime = (TextView) convertView
+					.findViewById(R.id.time);
 			tvTime.setText(alarm.toString());
 			convertView.setOnClickListener(this);
 			final View tvDel = convertView.findViewById(R.id.delpreset);
-   			tvDel.setOnClickListener(new View.OnClickListener(){
+			tvDel.setOnClickListener(new View.OnClickListener() {
 
-					public void onClick(View p2)
-					{
-						View p1 = (View)p2.getParent();
-						presetsList.removeView(p1);
-						for (AlarmClock alarm : presets)
-						{
-							if( alarm.getElement() == p1){
-								presets.remove(alarm);
-								DBHelper dbHelper = new DBHelper(getApplicationContext());
-								dbHelper.open();
-								dbHelper.deletePreset(alarm.getId());
-								dbHelper.close();
-								break;
-							}
-						}
-						historyMap.remove(p1);
-				
-				
-					}
+				public void onClick(View p2) {
+					View p1 = (View) p2.getParent();
+					presetsList.removeView(p1);
+					AlarmClock alarm = historyMap.get(p1);
+					presets.remove(alarm);
+					DBHelper dbHelper = new DBHelper(getApplicationContext());
+					dbHelper.open();
+					dbHelper.deletePreset(alarm.getId());
+					dbHelper.close();
+					historyMap.remove(p1);
 
-				
+				}
+
 			});
 			presetsList.addView(convertView, 0);
 			historyMap.put(convertView, alarm);
 
 			// On long click on preset - show edit dialog
-			convertView.setOnLongClickListener(new View.OnLongClickListener() { 
-					@Override
-					public boolean onLongClick(View v)
-					{
-						TimePickDialog timePickDialog = null;	
-						timePickDialog = new TimePickDialog(PresetsActivity.this);
-						timePickDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-						timePickDialog.setAlarm(alarm);
+			convertView.setOnLongClickListener(new View.OnLongClickListener() {
+				@Override
+				public boolean onLongClick(View v) {
+					TimePickDialog timePickDialog = null;
+					timePickDialog = new TimePickDialog(PresetsActivity.this);
+					timePickDialog
+							.requestWindowFeature(Window.FEATURE_NO_TITLE);
+					timePickDialog.setAlarm(alarm);
 
-						timePickDialog.setDialogResult(new TimePickDialog.OnMyDialogResult() {
-								public void finish(AlarmClock newAlarm)
-								{
+					timePickDialog
+							.setDialogResult(new TimePickDialog.OnMyDialogResult() {
+								public void finish(AlarmClock newAlarm) {
 									tvName.setText(alarm.getName());
 									tvTime.setText(alarm.toString());
 								}
 							});
-						timePickDialog.show();
-						return true;
-					}
-    			});
+					timePickDialog.show();
+					return true;
+				}
+			});
 
 		}
 
 		// show logs list
-		LinearLayout logsList = (LinearLayout)findViewById(R.id.logs_list);
+		LinearLayout logsList = (LinearLayout) findViewById(R.id.logs_list);
 		histories = dbHelper.getHistoryList();
-		for (AlarmClock alarm : histories)
-		{
-			String name = (alarm.getName() == null || alarm.getName().length() == 0) ?
-				"alarm": alarm.getName(); 	
+		for (AlarmClock alarm : histories) {
+			String name = (alarm.getName() == null || alarm.getName().length() == 0) ? "alarm"
+					: alarm.getName();
 			View convertView = inflater.inflate(R.layout.hist_unit, null);
 			TextView tvName = (TextView) convertView.findViewById(R.id.name);
 			tvName.setText(name);
@@ -131,66 +123,72 @@ public class PresetsActivity extends Activity implements OnClickListener
 		Intent mainActivity = new Intent(this, MainActivity.class);
 		mainActivity.putExtra("alarm_extra", "");
 		dbHelper.close();
-		
-		
-		 gestureDetector = new GestureDetector(new MyGestureDetector());
-        View logsScroll = findViewById(R.id.llViewLog);
+
+		gestureDetector = new GestureDetector(new MyGestureDetector());
+		View logsScroll = findViewById(R.id.llViewLog);
 		View presetsScroll = findViewById(R.id.llViewPreset);
 
-        // Set the touch listener for the main view to be our custom gesture listener
+		// Set the touch listener for the main view to be our custom gesture
+		// listener
 		logsScroll.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-				if (gestureDetector.onTouchEvent(event)) { return true; }
-                return false;
-            }});
+			public boolean onTouch(View v, MotionEvent event) {
+				if (gestureDetector.onTouchEvent(event)) {
+					return true;
+				}
+				return false;
+			}
+		});
 		presetsScroll.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-				if (gestureDetector.onTouchEvent(event)) { return true; }
-                return false;
-            }});
+			public boolean onTouch(View v, MotionEvent event) {
+				if (gestureDetector.onTouchEvent(event)) {
+					return true;
+				}
+				return false;
+			}
+		});
 
 	}
 
 	@Override
-	public void onClick(View v)
-	{
-		AlarmClock alarm = (AlarmClock)historyMap.get(v);
+	public void onClick(View v) {
+		AlarmClock alarm = (AlarmClock) historyMap.get(v);
 		Intent mainActivity = new Intent(this, MainActivity.class);
-		//	mainActivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		// mainActivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		mainActivity.putExtra("alarm_extra", alarm);
 		startActivity(mainActivity);
 
 	}
 
-	
 	class MyGestureDetector extends SimpleOnGestureListener {
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-			//Intent intent = new Intent(PresetsActivity.this.getBaseContext(), MainActivity.class);
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			// Intent intent = new Intent(PresetsActivity.this.getBaseContext(),
+			// MainActivity.class);
 
-            if (Math.abs(e1.getY() - e2.getY()) > MainActivity.SWIPE_MAX_OFF_PATH) {
-                return false;
-            }
-
-            // left to right swipe
-			if (e2.getX() - e1.getX() > MainActivity.SWIPE_MIN_DISTANCE && Math.abs(velocityX) > MainActivity.SWIPE_THRESHOLD_VELOCITY) {
-			 //startActivity(intent);
-			 PresetsActivity.this.finish();
-			 PresetsActivity.this.overridePendingTransition(
-			 R.anim.slide_in_left, 
-			 R.anim.slide_out_right
-			 );
+			if (Math.abs(e1.getY() - e2.getY()) > MainActivity.SWIPE_MAX_OFF_PATH) {
+				return false;
 			}
 
-            return false;
-        }
+			// left to right swipe
+			if (e2.getX() - e1.getX() > MainActivity.SWIPE_MIN_DISTANCE
+					&& Math.abs(velocityX) > MainActivity.SWIPE_THRESHOLD_VELOCITY) {
+				// startActivity(intent);
+				PresetsActivity.this.finish();
+				PresetsActivity.this.overridePendingTransition(
+						R.anim.slide_in_left, R.anim.slide_out_right);
+			}
 
-        // It is necessary to return true from onDown for the onFling event to register
-        @Override
-        public boolean onDown(MotionEvent e) {
+			return false;
+		}
+
+		// It is necessary to return true from onDown for the onFling event to
+		// register
+		@Override
+		public boolean onDown(MotionEvent e) {
 			return true;
-        }
+		}
 
-    }
+	}
 
 }
