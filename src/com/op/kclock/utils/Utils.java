10282c4e6870d2c3e5d52c319919731a82c01080
Alexander Pastukhov <picaro.vs@gmail.com>
2012-10-12 17:53:24 +0300

    Update src/com/op/kclock/utils/Utils.java
    save Prefs to SD

--- src/com/op/kclock/utils/Utils.java
+++ src/com/op/kclock/utils/Utils.java
@@ -84,4 +84,77 @@ public class Utils {
 		return Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
 	}
 
+
+private boolean saveSharedPreferencesToFile(File dst) {
+    boolean res = false;
+    ObjectOutputStream output = null;
+    try {
+        output = new ObjectOutputStream(new FileOutputStream(dst));
+        SharedPreferences pref = 
+                            getSharedPreferences(prefName, MODE_PRIVATE);
+        output.writeObject(pref.getAll());
+
+        res = true;
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }finally {
+        try {
+            if (output != null) {
+                output.flush();
+                output.close();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    return res;
+}
+
+@SuppressWarnings({ "unchecked" })
+private boolean loadSharedPreferencesFromFile(File src) {
+    boolean res = false;
+    ObjectInputStream input = null;
+    try {
+        input = new ObjectInputStream(new FileInputStream(src));
+            Editor prefEdit = getSharedPreferences(prefName, MODE_PRIVATE).edit();
+            prefEdit.clear();
+            Map<String, ?> entries = (Map<String, ?>) input.readObject();
+            for (Entry<String, ?> entry : entries.entrySet()) {
+                Object v = entry.getValue();
+                String key = entry.getKey();
+
+                if (v instanceof Boolean)
+                    prefEdit.putBoolean(key, ((Boolean) v).booleanValue());
+                else if (v instanceof Float)
+                    prefEdit.putFloat(key, ((Float) v).floatValue());
+                else if (v instanceof Integer)
+                    prefEdit.putInt(key, ((Integer) v).intValue());
+                else if (v instanceof Long)
+                    prefEdit.putLong(key, ((Long) v).longValue());
+                else if (v instanceof String)
+                    prefEdit.putString(key, ((String) v));
+            }
+            prefEdit.commit();
+        res = true;         
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+    }finally {
+        try {
+            if (input != null) {
+                input.close();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    return res;
+}
+
+
 }
